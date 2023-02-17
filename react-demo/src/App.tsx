41861221288f2ca6bd8cd1d5df2932c3a66ee7ac
Zhivaev Artem <zhivaev993@gmail.com>
2023-02-18 02:09:19 +0300

    Init

---
+++ react-demo/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.module.scss';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      Sample text
+    </div>
+  );
+}
+
+export default App;
---
+++ react-demo/src/types/index.d.ts
@@ -0,0 +1,14 @@
+declare module '*.module.scss' {
+    const content: Record<string, string>;
+    export default content;
+}
+
+declare module '*.module.css' {
+    const content: Record<string, string>;
+    export default content;
+}
+
+declare module '*.module.less' {
+    const content: Record<string, string>;
+    export default content;
+}
